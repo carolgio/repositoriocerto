ddb286c73a524bdbe45465b50fec2addff8dfd98
carolgio <108530479+carolgio@users.noreply.github.com>
2024-05-06 11:07:47 -0300

    s

--- main.js
+++ main.js
@@ -185,7 +185,7 @@ for (let i = 0; i < contadores.length; i++) {
 
 // definição do tempo dos objetivos
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-04-30T00:00:00");
+const tempoObjetivo1 = new Date("2024-10-30T00:00:00");
 const tempoObjetivo2 = new Date("2024-10-15T00:00:00");
 const tempoObjetivo3 = new Date("2024-12-06T00:00:00");
 const tempoObjetivo4 = new Date("2025-12-30T00:00:00");
